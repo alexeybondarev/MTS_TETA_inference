af0953b6ad57a68a992c82ffe31d6c19beb9d941
Alexey Bondarev <alxbondarev@gmail.com>
2021-08-24 17:58:28 +0300

    streamlit

--- SPAM_app.py
+++ SPAM_app.py
@@ -1,11 +1,21 @@
 from SPAM_classifier import SpamClassifier
+import streamlit as st
 
-text_input = "free entri in 005 text fa to 87121 to receiv entri"
-model = SpamClassifier()
-label = model.predict_text(text_input)
-if label == 0:
-    print("That is not a SPAM")
-elif label == 1:
-    print("SPAM detected!")
-else:
-    print("Oops, something went wrong")
+st.title('SPAM SMS classification DEMO by fit-predict team')
+
+st.markdown('Type or paste SMS message below')
+
+with st.form('form'):
+    text_input = st.text_area('Enter SMS message here: ', "I don't think this message is a SPAM")
+    submit_button = st.form_submit_button('SPAM or not SPAM?')
+    
+    if submit_button:
+        model = SpamClassifier()
+        label = model.predict_text(text_input)
+        if label == 0:
+            st.write("That SMS message is not a SPAM")
+            print("That is not a SPAM")
+        elif label == 1:
+            st.write("SPAM detected!")
+        else:
+            st.write("Oops, something went wrong")
